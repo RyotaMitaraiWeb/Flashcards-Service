ebee81fe8eac075ac78f8832703c07d1730b2f66
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-04-01 19:59:01 +0300

    configure swagger for JWT authentication and global pipelines for validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger/dist';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
+import 'reflect-metadata';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  
   app.enableCors({
     origin: process.env.CORS_ORIGIN,
   });
@@ -16,7 +21,14 @@ async function bootstrap() {
       .setDescription('API for Flashcards application')
       .setVersion('1.0')
       .addTag('flashcards')
-      .addBearerAuth()
+      .addBearerAuth({
+        description: 'Bearer JWT',
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header'
+      }, 'jwt')
       .build();
 
     const document = SwaggerModule.createDocument(app, swagger);
@@ -24,6 +36,7 @@ async function bootstrap() {
   }
 
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
