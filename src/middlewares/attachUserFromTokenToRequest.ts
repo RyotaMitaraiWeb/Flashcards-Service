1429ebc9738945958b8644582fb84f6511e0ba92
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-04-07 00:12:55 +0300

    add AttachUserFromTokenToRequest middleware

---
+++ src/middlewares/attachUserFromTokenToRequest.ts
@@ -0,0 +1,35 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { IRequest, IUser } from '../interfaces';
+import { NextFunction } from 'express';
+import { extractTokenFromHeader } from '../util/extractTokenFromHeader/extractTokenFromHeader';
+import { JwtService } from '@nestjs/jwt';
+
+/**
+ * Attaches an ``IUser`` object to the request for any valid JWTs in the ``Authorization`` header
+ * or ``null`` if the JWT is invalid.
+ * 
+ * **Note:** this middleware does not check if a JWT is blacklisted. To prevent unauthorized access
+ * from invalid JWTs, use the ``IsLoggedIn`` guard.
+ */
+@Injectable()
+export class AttachUserFromTokenToRequestMiddleware implements NestMiddleware {
+  constructor(
+    private readonly jwtService: JwtService
+  ) { }
+  async use(req: IRequest, _res: Response, next: NextFunction) {
+    const bearerToken = req.headers.authorization;
+    const jwt = extractTokenFromHeader(bearerToken);
+    
+    try {
+      const user: IUser = await this.jwtService.verifyAsync(jwt, {
+        secret: process.env.JWT_SECRET
+      });
+
+      req.user = user;
+    } catch {
+      req.user = null;
+    }
+
+    next();
+  }
+}
