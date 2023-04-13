cb693ce98094b5600bd1508e3b6d098a18623124
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-04-13 18:27:03 +0300

    update invalidActionsMessages with bookmark actions

--- src/constants/invalidActionsMessages.ts
+++ src/constants/invalidActionsMessages.ts
@@ -6,5 +6,8 @@ export const invalidActionsMessages = {
   isNotLoggedOut: 'You must be logged out to perform this action',
   isNotLoggedIn: 'You must be logged in to perform this action',
   isNotCreator: 'You must be the creator of the deck to perform this action',
+  isCreator: 'You cannot perform this action because you are the creator of the deck',
   deckDoesNotExist: 'The deck you are looking for does not exist',
+  hasAlreadyBookmarked: 'You have already bookmarked this deck',
+  hasNotBookmarked: 'You have not bookmarked this deck'
 }
